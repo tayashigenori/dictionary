8048f1d4153e2386de6b78d339bd6a582bd495d8
Shigenori Taya <shigenori.taya@gmail.com>
2014-10-24 02:01:11 +0900

    adding unihan variants

--- unihan/html_parser.py
+++ unihan/html_parser.py
@@ -65,6 +65,17 @@ def store_dictionary(dictname, readings):
     finally:
         f_d.close()
 
+def store_variants(varname, variants):
+    f_v = open(varname, 'w+')
+    try:
+        for codepoint,v in variants.items():
+            d  = [codepoint]
+            d += [json.dumps(v)]
+            f_v.write(VAR_SEPARATOR.join(d))
+            f_v.write("\n")
+    finally:
+        f_v.close()
+
 def get_readings_from_source_url(source_url_name):
     res = {}
     f_s = open(source_url_name,)
@@ -81,20 +92,40 @@ def get_readings_from_source_url(source_url_name):
         f_s.close()
     return res
 
+def get_variants_from_source_url(source_url_name):
+    res = {}
+    f_s = open(source_url_name,)
+    try:
+        for url in f_s.readlines():
+            sys.stderr.write("getting resource: %s\n" %url)
+            codepoint = url.split("codepoint=")[1].strip()
+            page = UnihanPage(url)
+            res[codepoint] = page.get_variants()
+    except:
+        sys.stderr.write("could not get resource: %s\n" %url)
+        pass
+    finally:
+        f_s.close()
+    return res
+
+
 def main():
     # urls are supposed to be stored in file
     #UNIHAN_URL_BASE = "http://www.unicode.org/cgi-bin/GetUnihanData.pl?codepoint=%s"
     #codepoint = "4E00"
     #target_url = UNIHAN_URL_BASE %(codepoint)
     #pages = [target_url]
-    STROKES_UPPER = 21
-    for stroke in range(12, STROKES_UPPER):
+    STROKES_UPPER = 65
+    for stroke in range(5, STROKES_UPPER):
         # get url and parse
         source_url_name = SOURCE_URL_PATH_BASE %(stroke)
-        readings = get_readings_from_source_url(source_url_name)
+        #readings = get_readings_from_source_url(source_url_name)
+        variants = get_variants_from_source_url(source_url_name)
         # store
-        dictname = DICT_PATH_BASE %(stroke)
-        store_dictionary(dictname, readings)
+        #dictname = DICT_PATH_BASE %(stroke)
+        #store_dictionary(dictname, readings)
+        varname = VAR_PATH_BASE %(stroke)
+        store_variants(varname, variants)
 
 if __name__ == '__main__':
     main()
